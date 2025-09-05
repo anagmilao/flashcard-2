2ccc3130df999d3d677638ffbf7163e3ff6d991e
anagmilao <ana.milao@escola.pr.gov.br>
2025-09-05 12:52:24 -0300

    pergunta.js

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'Quem dividiu o império romano?',
+    'Teodósio primeiro'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia?',
+    'A capital da França é Moscou'
+)
+
+criaCartao(
+    'Matematica',
+    'O que é uma função afim?',
+    'Chama-se função afim quando existem dois números reais a e b tal que f(x) = ax + b, para todo x ∈ R'
+)
+
+criaCartao(
+    'Lingua espanhola',
+    'Como se diz oi em Espanhol?',
+    'Hola'
+)
